b798b7b2e6d4d6f0280b302ea64e237b95ec6022
hoowolf <168746728+hoowolf@users.noreply.github.com>
2025-12-05 12:42:29 +0800

    Change response message to 'Test SHA!'

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Cloud Computing Class!');
+  res.end('Test SHA!');
 });
 
 server.listen(3000, () => {
